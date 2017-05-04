25296b3bc26deac53b324dd7f948d5500c8e2c24
Rutul Patel <rutul-patel@live.com>
2017-05-03 18:45:19 -0700

    Add to-eat & eaten controller functions

--- app/public/assets/js/burger-controller.js
+++ app/public/assets/js/burger-controller.js
@@ -1,3 +1,42 @@
 $(document).ready(function() {
 
+    $("#submitBtn").on("click", function() {
+        alert("clicked");
+    });
+
+    function loadToEatData() {
+        $.get("/api/all?devoured=false", function(data) {
+            console.log(data);
+
+            for (var i = 0; i < data.length; i++) {
+
+                var div = $("<div>");
+                var input = $("<input>")
+                    .attr({ "type": "text", "disabled": "", "value": data[i].burger_name });
+                var button = $("<button>")
+                    .attr({ "class": "btn btn-danger btn-sm" }).text("Devour it!");
+
+                div.html(input).append(button);
+                $("#to-eat-container").append(div);
+            }
+        });
+    }
+
+    function loadEatenData() {
+        $.get("/api/all?devoured=true", function(data) {
+            console.log(data);
+
+            for (var i = 0; i < data.length; i++) {
+
+                var div = $("<div>");
+                var input = $("<input>")
+                    .attr({ "type": "text", "disabled": "", "value": (i + 1) + ". " + data[i].burger_name });
+                div.html(input);
+                $("#eaten-container").append(div);
+            }
+        });
+    }
+
+    loadToEatData();
+    loadEatenData();
 });
