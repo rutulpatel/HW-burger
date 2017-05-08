b582af9368ab1d4b3f4c4ca4a413c74b102adfe0
Rutul Patel <rutul-patel@live.com>
2017-05-08 14:33:44 -0700

    Update connection.js to work with JAWSDB heroku

--- app/config/connection.js
+++ app/config/connection.js
@@ -1,11 +1,17 @@
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'burgers_db'
-});
+
+// Heroku Jaws DB connection
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    var connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'burgers_db'
+    });
+}
 
 connection.connect(function(err) {
     if (err) throw err;
